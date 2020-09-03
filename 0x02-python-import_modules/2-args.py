17f9e83547908df0683402d4ed14a9dc3ca68cf1
onsjannet <onsjannet@gmail.com>
2020-09-03 21:21:53 +0100

    added script to 3rd fiel

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    l = len(sys.argv) - 1
+    if l == 0:
+        print("{:d} arguments.".format(l))
+    elif l == 1:
+        print("{:d} arguments.".format(l))
+    else:
+        print("{:d} arguments:".format(l))
+    for i in range(l):
+        print("{:d}: {}".format(i + 1, sys.argv[i + 1]))
