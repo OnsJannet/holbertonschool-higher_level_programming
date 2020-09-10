3881a23daeb6976655d5f55e0578758338ab6f2b
onsjannet <onsjannet@gmail.com>
2020-09-10 22:06:55 +0100

    corrected scipt

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for number in my_list:
-        if number % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
+def best_score(a_dictionary):
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return sorted(a_dictionary)[-1]
-    else:
-    	return None
+        return max(a_dictionary, key=a_dictionary.get)
