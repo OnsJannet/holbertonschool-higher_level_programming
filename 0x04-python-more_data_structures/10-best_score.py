61b614a4010ca7cf7a28b51fc5917dac830dd273
onsjannet <onsjannet@gmail.com>
2020-09-10 21:39:55 +0100

    added condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-        if a_dictionary:
-            return sorted(a_dictionary)[-1]
+    if a_dictionary:
+        return sorted(a_dictionary)[-1]
+    else:
+    	return None
