999f4a4ee677a26f23004d7094444de7371764ea
onsjannet <onsjannet@gmail.com>
2020-09-10 22:24:48 +0100

    corrected pep8

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,9 @@
 def weight_average(my_list=[]):
     i, j = 0, 0
     if my_list:
-    	tupl = ()
-    	for tupl in my_list:
-    		i += (tupl[0] * tupl [1])
-    		j += tupl[1]
-    	return i / j
+        tupl = ()
+        for tupl in my_list:
+            i += (tupl[0] * tupl[1])
+            j += tupl[1]
+        return i / j
     return 0
