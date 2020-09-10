1881e2a8e0b414e008a3f13e2cdc127422e26afb
onsjannet <onsjannet@gmail.com>
2020-09-10 22:22:15 +0100

    added script

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    i, j = 0, 0
+    if my_list:
+    	tupl = ()
+    	for tupl in my_list:
+    		i += (tupl[0] * tupl [1])
+    		j += tupl[1]
+    	return i / j
+    return 0
