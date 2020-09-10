b63f3b8b6869deb38fb8766494d5b2d08939752d
onsjannet <onsjannet@gmail.com>
2020-09-10 18:56:02 +0100

    corrected pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if my_list[number] == search else my_list[number]
-    for number in range(len(my_list))]
+        return [replace if number == search else number for number in my_list]
