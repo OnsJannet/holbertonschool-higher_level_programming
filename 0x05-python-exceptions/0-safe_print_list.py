1bb5b78622d898a43bed865cc8734f0e240a9697
onsjannet <onsjannet@gmail.com>
2020-09-21 17:24:54 +0100

    corrected pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	count = my_list.count('0')
-	for i in range (0, x):
-		try:
-			print("{}".format(my_list[i]), end='')
-			count += 1
-		except:
-			break
-	print()
-	return count
+    count = my_list.count('0')
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            count += 1
+        except Exception:    # except Exception
+                            # to silince pep8 : E722 do not use bare except'
+            break
+    print()
+    return count
