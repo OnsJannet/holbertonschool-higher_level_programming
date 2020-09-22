14957ce6a9136ed9212e5e26523abf7563b29c12
onsjannet <onsjannet@gmail.com>
2020-09-22 18:18:13 +0100

    added comment

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 class Square:
+    """defines a square and checks if its a digit >= 0"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
