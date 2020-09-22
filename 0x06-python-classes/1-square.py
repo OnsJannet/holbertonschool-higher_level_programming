49438d65cca1c5e71004146e6c0e2b51b2eb3d58
onsjannet <onsjannet@gmail.com>
2020-09-22 18:23:56 +0100

    added comment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""Defining a class (Square)"""
+
+
 class Square:
+    """Square class
+    Attributes:
+    (size): the size of the square"""
     def __init__(self, size):
+        """size: size for __size attribute of instance class"""
         self.__size = size  # this attribute is private
