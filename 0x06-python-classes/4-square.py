850ea7036b5deac9725f0e022d876414db1da89a
onsjannet <onsjannet@gmail.com>
2020-09-22 18:51:43 +0100

    corrected pep8 and added comments

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defining a class (Square)"""
+
+
+class Square:
+    """Square class
+    Attributes:
+    (size): the size of the square
+    """
+    def __init__(self, size=0):
+        """
+        size: size for __size attribute of instance class
+        """
+        self.__size = size  # this attribute is private
+
+    def area(self):
+        """ this helps calculate the area of the square
+        returns the area of the square
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """returns the size of the class instance"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
