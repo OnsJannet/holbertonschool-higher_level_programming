788e8db9e6db32e7c2889a006ca1933bd4eda70e
onsjannet <onsjannet@gmail.com>
2020-09-25 20:30:37 +0100

    keep getting an error so i keep changing it

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-
 def add_integer(a, b):
-    """function add_integer returns the sum of two integers
-    isinstance : checks
+    """
+    add_integer - Adss Two Integers
+    @a: where Type a must be an integer
+    @b: where Type b must be an integer
     """
     if isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return int(a) + int(b)
     else:
         raise TypeError("{:} must be an integer"
                         .format('b' if isinstance(a, (int, float)) else 'a'))
+
