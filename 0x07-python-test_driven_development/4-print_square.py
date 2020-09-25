5c4dcfdaf542bcf22aabadd745268f819410b0b4
onsjannet <onsjannet@gmail.com>
2020-09-25 21:19:08 +0100

    added a script that prints a square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def print_square(size):
+    """
+    print_square- Prints a Square
+    @Size: where Type size must be an integer and must < 0
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
