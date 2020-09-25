be773f991adfd7774205d443aab7e5260af487bb
onsjannet <onsjannet@gmail.com>
2020-09-25 21:57:14 +0100

    a script that prints a paragraph

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    """
+    text_identation - Prints a text with 2 new lines after : . ?
+    @text: where Type text must be a str
+    """
+
+    if text == "":
+        raise TypeError('text must be a string')
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    for i in ['.', '?', ':']:
+    	text = text.replace(i, i + '\n\n')
+    new = [line.strip() for line in text.split('\n')]
+    new = '\n'.join(new)
+    print(new, end='')
+
