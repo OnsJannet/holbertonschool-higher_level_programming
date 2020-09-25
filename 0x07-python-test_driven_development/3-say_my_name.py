890d90e561ce75a680e16ebf9ae00479659eb853
onsjannet <onsjannet@gmail.com>
2020-09-25 21:20:07 +0100

    corrected pep8

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name - Prints The Name And The Last Name
+    @first_name: where Type first_name must be a string
+    @last_name: where Type last_name must be a string
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    elif type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    else:
+        print("My name is {} {}".format(first_name, last_name))
