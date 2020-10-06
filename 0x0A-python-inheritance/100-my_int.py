a6256d85e3f1d265adfb1f1f56b0b5750926e570
onsjannet <onsjannet@gmail.com>
2020-10-06 22:42:05 +0100

    changed code

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,14 +5,10 @@ Class MyInt inverts operators
 
 
 class MyInt(int):
+    """Inherit Class"""
+
     def __eq__(self, other):
-        """
-        to change the == sign to !=
-        """
         return int(self) != int(other)
 
     def __ne__(self, other):
-        """
-        to Changes  the != to ==
-        """
         return int(self) == int(other)
