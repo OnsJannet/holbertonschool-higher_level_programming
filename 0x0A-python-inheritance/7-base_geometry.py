953c56c0091e3dcf38deec147a7817be07de0cef
onsjannet <onsjannet@gmail.com>
2020-10-06 18:25:47 +0100

    added a class that raises a TypeError - ValueError exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,9 +4,19 @@ BaseGeometry
 """
 
 
-class BaseGeometry():
+class BaseGeometry:
+    """
+    An class named BaseGeometry
+    attr1(area): Raises an exception
+    """
+
     def area(self):
-        """
-        Raises an Exception
-        """
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Raises an Exception"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} <name> must be greater than 0".format(name))
