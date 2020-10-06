42ae2d340079d79084f70c780b359e5136dc159e
onsjannet <onsjannet@gmail.com>
2020-10-06 19:55:36 +0100

    changed doc

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-a function that prints a sorted list inherited from list
-"""
+"""a function that prints a sorted list inherited from list"""
 
 
 class MyList(list):
-
-    """
-    prints a sorted list in an ascending order
-    """
+    """prints a sorted list in an ascending order"""
     def print_sorted(self):
-        """
-        Prints a list in ascending order
-        """
+        """Prints a list in ascending order"""
         print(sorted(self))
