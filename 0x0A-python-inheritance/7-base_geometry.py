94aa47505c2b9647a0ac6e67624b437aac63178f
onsjannet <onsjannet@gmail.com>
2020-10-06 21:08:34 +0100

    pep8

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,6 @@ class BaseGeometry:
     An class named BaseGeometry
     attr1(area): Raises an exception
     """
-
     def area(self):
         """Raises an Exception"""
         raise Exception("area() is not implemented")
