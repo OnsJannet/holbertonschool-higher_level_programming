4748d545fede3383c278d1669ecfc5707cd897a7
onsjannet <onsjannet@gmail.com>
2020-10-06 18:28:25 +0100

    added a class that raises a TypeError - ValueError exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,4 +19,4 @@ class BaseGeometry:
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} <name> must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
