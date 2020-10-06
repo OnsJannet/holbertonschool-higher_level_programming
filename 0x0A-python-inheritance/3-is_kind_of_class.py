eaaa7ceb43a87bce0263971bdf8b773e8812ce84
onsjannet <onsjannet@gmail.com>
2020-10-06 13:33:33 +0100

    added a function that Checks if obj is instance of a specified class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a function that checks if the object is an instance of the
+specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    a function that Checks if obj is instance of a specified class
+    obj: Object
+    a_class: Class
+    return: True if is exactly instance, False otherwise
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
