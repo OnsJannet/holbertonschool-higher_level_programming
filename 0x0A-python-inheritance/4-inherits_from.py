c61338354a9fdbd4ca5902ff388b59a8ba6d0926
onsjannet <onsjannet@gmail.com>
2020-10-06 18:05:16 +0100

    added files

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 a function that checks if the object is an instance of the
-specified class
+specified class or not
 """
 
 
@@ -10,7 +10,8 @@ def inherits_from(obj, a_class):
     a function that Checks if obj is instance of a specified class
     obj: Object
     a_class: Class
-    return: True if is exactly instance, False otherwise
+    return: True if directly or inderictely inherited from the
+    specified class
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+BaseGeometry
+"""
+
+
+class BaseGeometry():
+    def area(self):
+        """
+        Raises an Exception
+        """
+        raise Exception("area() is not implemented")
