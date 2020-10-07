6229cfc28feb72359c5de256c05c495b045371e0
onsjannet <onsjannet@gmail.com>
2020-10-07 18:51:35 +0100

    added a file that returns a rep of an object

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that return the JSON rep of an object str"""
+
+import json
+def to_json_string(my_obj):
+    """return the JSON rep of an object
+    my_obj: obj
+    return: rep of an object
+    """
+
+    return json.dumps(my_obj)
