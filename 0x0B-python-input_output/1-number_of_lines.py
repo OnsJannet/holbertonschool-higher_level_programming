3801dd0971e625e4d6a57368fcb1c9a8f87888d2
onsjannet <onsjannet@gmail.com>
2020-10-07 20:43:35 +0100

    added com

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -6,6 +6,9 @@ of lines of a text file
 
 
 def number_of_lines(filename=""):
+	    """Returns the number of lines in a text file
+    filename (str): Filename
+    """
     with open(filename) as my_file:
         lines = len(my_file.readlines())
     return lines
