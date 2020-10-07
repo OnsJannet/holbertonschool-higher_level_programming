d9a1af7492d497c161b931bbc7124a160c8e8971
onsjannet <onsjannet@gmail.com>
2020-10-07 20:50:43 +0100

    removed mode read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     filename: Filename
     """
 
-    with open(filename, mode="r", encoding='utf-8') as my_file:
+    with open(filename, encoding='utf-8') as my_file:
         print(my_file.read(), end="")
