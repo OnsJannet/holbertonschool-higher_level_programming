24f8d39c243d64e2041480e9ab921b07d2cc83e6
onsjannet <onsjannet@gmail.com>
2020-10-07 20:54:59 +0100

    removed blank lines

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ def read_file(filename=""):
     """Reads a text file and prints it
     filename: Filename
     """
-
     with open(filename, encoding='utf-8') as my_file:
         print(my_file.read(), end="")
