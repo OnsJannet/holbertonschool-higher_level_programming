f2e044a161c92f7c48f7f664c5fdd2deede17cf5
onsjannet <onsjannet@gmail.com>
2020-10-07 21:31:59 +0100

    pep8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a text file and prints it
-    filename: Filename
+    """
+    Reads a text file and prints it
+    filename: filename
     """
     with open(filename, encoding='UTF8') as my_file:
         print(my_file.read(), end="")
