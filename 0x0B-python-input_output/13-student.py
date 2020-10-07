d11614d585106174a91dc81e0892d1ca8ab97da4
onsjannet <onsjannet@gmail.com>
2020-10-07 20:36:37 +0100

    reloads instances of student from a jason

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This modle creates a class named Student"""
+
+
+class Student:
+    """Class named Student
+    first_name: the first name of a student
+    last_name: the last name of a student
+    age: the age of a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student"""
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                dictionary[i] = self.__dict__[i]
+        return dictionary
+
+    def reload_from_json(self, json):
+        """Reloads instances of Student from a json"""
+        for k, value in json.items():
+            self.__dict__[k] = value
