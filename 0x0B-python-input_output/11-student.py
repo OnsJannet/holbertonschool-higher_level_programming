0e23aea66909261dc67182b276a432373e298a18
onsjannet <onsjannet@gmail.com>
2020-10-07 20:16:58 +0100

    pep8

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""This modle creates a class named Student"""
 
 
 class Student:
@@ -9,7 +9,7 @@ class Student:
     age: the age of a student
     """
     def __init__(self, first_name, last_name, age):
-        """Init an instance"""
+        """Initializes an instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
