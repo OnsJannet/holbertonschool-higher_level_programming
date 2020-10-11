4870e566ad865bf9ea63953de7c46f56fc35677e
onsjannet <onsjannet@gmail.com>
2020-10-11 14:02:39 +0100

    added height width x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,15 +4,14 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Rectangle class inheriting from Base class"""
-
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes args
         width is the width of the rectangle
         height is the height of the rectangle
-        x is x 
+        x is x
         y is y
         """
         super().__init__(id)
@@ -20,3 +19,59 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """Returns width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width of class rectangle"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height of class rectangle"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Returns x of rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x of class rectangle"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Returns y of rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y of class rectangle"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
