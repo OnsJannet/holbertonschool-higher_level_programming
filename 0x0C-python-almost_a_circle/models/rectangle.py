317e7e5ab2b6fcb69a9aa8a11107f744d463b649
onsjannet <onsjannet@gmail.com>
2020-10-11 12:57:04 +0100

    added a rectangle class that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" Base"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class inheriting from Base class"""
+
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes args
+        width is the width of the rectangle
+        height is the height of the rectangle
+        x is x 
+        y is y
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
