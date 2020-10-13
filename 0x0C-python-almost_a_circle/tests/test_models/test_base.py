28a66d03870080502782dc32855f75de9f6527c8
onsjannet <onsjannet@gmail.com>
2020-10-13 18:26:28 +0100

    added pep8 test to other files

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Contains tests for Base class
+"""
+
+import unittest
+import inspect
+import pep8
+import json
+from models.square import Square
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestBaseDocs(unittest.TestCase):
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test(self):
+        """checks test conformity to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,20 +7,22 @@ import unittest
 import inspect
 import pep8
 import json
+from models.square import Square
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBaseDocs(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
+        result = pep8style.check_files(['models/rectangle.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test(self):
         """checks test conformity to PEP8."""
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        result = pep8style.check_files(['tests/test_models/test_rectangle.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
