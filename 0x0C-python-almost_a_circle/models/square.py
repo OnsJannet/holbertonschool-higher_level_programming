2c1bd1a8a5b25c3b29239f4a24c2ed5e509e26d9
onsjannet <onsjannet@gmail.com>
2020-10-13 04:10:07 +0100

    added class square that inherits from rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python3
-""" square Module"""
+"""This module creates the Square class that inherits from Rectangle"""
 
 
 from models.rectangle import Rectangle
 
 
-class Squre(Rectangle):
-	"""Square class inheriting from Rectangle class"""
+class Square(Rectangle):
+    """A class named Square
+    Attributes:
+    attr1(id): id of object
+    attr2(width): square width
+    attr3(height): square height
+    attr4(x): number of spaces before square
+    attr5(y): number of spaces before square
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes instance of Square class"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns string representation of Square """
+        return"[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
