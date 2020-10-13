218e51ec393eedd8575d34a05e7c07ed3a7ffa14
onsjannet <onsjannet@gmail.com>
2020-10-13 04:14:26 +0100

    changed comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,16 +6,14 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """A class named Square
-    Attributes:
-    attr1(id): id of object
-    attr2(width): square width
-    attr3(height): square height
-    attr4(x): number of spaces before square
-    attr5(y): number of spaces before square
-    """
+    """Square class inheriting from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes instance of Square class"""
+        """Initializes instance of Square class
+        width is the width of the rectangle
+        height is the height of the rectangle
+        x is x
+        y is y
+        """
         super().__init__(size, size, x, y, id)
 
     @property
