fb03106ddb134908fb7fd517b37c5b47c60f6625
onsjannet <onsjannet@gmail.com>
2020-10-13 12:44:41 +0100

    correcting output 7

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,8 +41,8 @@ class Base:
     def from_json_string(json_string):
         """returns the list of the JSON string representation"""
         if json_string is None:
-            return loads()
-        return loads(json_string)
+            return loads([])
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
