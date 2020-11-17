8aa04943ce4fd4a2b9ba2181e301bb179fcafd07
onsjannet <onsjannet@gmail.com>
2020-11-17 18:42:50 +0100

    added a file that creates a datbase and user and grants the user SELECT perm

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user user_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON database_name.hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
