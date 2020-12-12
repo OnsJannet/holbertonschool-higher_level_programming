847d188c089785bb1ca3ad15b1f3170f9090612e
onsjannet <onsjannet@gmail.com>
2020-12-12 16:39:50 +0100

    chmod

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,12 +3,14 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-c = db.cursor()
-c.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-for state in c.fetchall():
-    print(state)
-c.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
