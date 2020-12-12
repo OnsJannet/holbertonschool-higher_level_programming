971b848f8f830577b0ba7b666af6f1b9be9b1106
onsjannet <onsjannet@gmail.com>
2020-12-12 16:16:06 +0100

    added localhost

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,11 +3,13 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=argv[1],
+db = MySQLdb.connect(host="localhost", 
+                     port=3306,
+                     user=argv[1],
                      passwd=argv[2],
                      db=argv[3])
 c = db.cursor()
-c.execute("SELECT * FROM states WHERE name ORDER BY ASC")
+c.execute("SELECT * FROM states WHERE id ORDER BY ASC")
 for state in c.fetchall():
     print(state)
 c.close()
