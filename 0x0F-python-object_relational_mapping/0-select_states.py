06bd6a75df45d1ac36ca853aa1c5e98c18e5fc3f
onsjannet <onsjannet@gmail.com>
2020-12-12 16:39:08 +0100

    pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """ Python x MySQL : Listing data from a database using args """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=argv[1],
-                     passwd=argv[2],
-                     db=argv[3])
-c = db.cursor()
-c.execute("SELECT * FROM states WHERE ORDER BY id ASC")
-for state in c.fetchall():
-    print(state)
-c.close()
-db.close()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE ORDER BY id ASC")
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
