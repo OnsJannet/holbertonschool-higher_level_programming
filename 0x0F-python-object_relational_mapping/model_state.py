213e94acc3c7150c3a5f11e8685a72dcba523509
onsjannet <onsjannet@gmail.com>
2020-12-13 10:13:00 +0100

    pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,9 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     ''' class State that inherits from Base '''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
-    name =  Column(String(128), nullable=False)
-    
+    name = Column(String(128), nullable=False)
