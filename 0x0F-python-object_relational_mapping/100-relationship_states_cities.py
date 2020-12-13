ba090ccbe02995712a6cd0ab1bb04b8afc499570
onsjannet <onsjannet@gmail.com>
2020-12-13 19:08:55 +0100

    added a script that creates a relationship between tables

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = State(name="California", cities=[City(name="San Francisco")])
+    state = State(name="California", cities=City(name="San Francisco"))
     session.add(state)
     session.commit()
     session.close()
