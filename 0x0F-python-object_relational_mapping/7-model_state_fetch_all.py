057f4fc6000f3b1236a152fd072f4b7f0269c720
onsjannet <onsjannet@gmail.com>
2020-12-13 16:10:52 +0100

    added script that lists data from table using SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Python x SQLAlchemy  : Listing data from table """
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
