ee925b3b3f5044aded075136f48dff68b868b44a
onsjannet <onsjannet@gmail.com>
2021-01-11 14:25:49 +0100

    correcting output

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,9 +7,9 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    data = {'email': argv[2]}
+    data = {'email' : argv[2]}
     data = parse.urlencode(data)
     data = data.encode('ascii')
-    req = request.Request(url, data)
-    with request.urlopen(req) as response:
-        print(res.read().decode('utf-8'))
+    r = request.Request(url, data)
+    with request.urlopen(r) as response:
+        print(response.read().decode('utf-8'))
