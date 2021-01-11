8577b588f682da853dda899fdd9d750b2c7ab5eb
onsjannet <onsjannet@gmail.com>
2021-01-11 13:24:35 +0100

    added a script that sends a request and display a response

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" a function that fetches a URL """
+
+import urllib.request as request
+import urllib.error as error
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(request.Request(argv[1])) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+            print("Error code: {}".format(err.code))
