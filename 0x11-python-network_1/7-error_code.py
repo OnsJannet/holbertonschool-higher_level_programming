82d8c9a7f60deeb0f5a7645b8343aaf62b84c249
onsjannet <onsjannet@gmail.com>
2021-01-11 14:08:16 +0100

    added a script that returns an error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" a function that Sends a request and display a response """
+import requests
+from requests.exceptions import HTTPError
+    r = requests.get(argv[1])
+        if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
