38af93e4794b0de77805ee35c6959f8b2642dd75
onsjannet <onsjannet@gmail.com>
2021-01-11 16:52:55 +0100

    added a script that request a isplay a response

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" a function that Sends a request and display a response """
+import requests
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) > 1:
+        q = argv[1]
+    else:
+        q = ""
+data = {'q': q}
+try:
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data).json()
+    if 'id' in r and 'name' in r:
+        print("[{}] {}".format(r['id'], r['name']))
+    else:
+        print("No result")
+except ValueError:
+    print("Not a valid JSON")
