2a08cbb139063afbeed75aa3a7cab1b277bd456e
onsjannet <onsjannet@gmail.com>
2021-01-11 13:38:12 +0100

    added a script that fetches a URL using request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" a function that fetches a URL """
+import requests
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
