c263f428c02992aa7122caf3829fd3b4e5280a0a
onsjannet <onsjannet@gmail.com>
2021-01-11 13:02:01 +0100

    added a script that fetches a url

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" a function that fetches a URL """
+
+import urllib.request
+from sys import argv
+if __name__ == "__main__":
+    with urllib.request.urlopen(urllib.request.Request(argv[1])) as response:
+        print(response.headers.get('X-Request-Id'))
