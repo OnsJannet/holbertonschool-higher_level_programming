22aa968256ae36dbd081d273eb87f5fa364cc139
onsjannet <onsjannet@gmail.com>
2021-01-11 18:19:51 +0100

    added a script that display id using github credentials

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,6 @@ from sys import argv
 
 if __name__ == "__main__":
 
-        r = requests.get('https://api.github.com/user', auth=Basic(
+        r = requests.get('https://api.github.com/user', auth=(
             argv[1], argv[2])).json()
         print(r.get('id'))
