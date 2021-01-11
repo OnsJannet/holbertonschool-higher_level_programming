03b084454b12b16d167ea0643cd2780c66d49873
onsjannet <onsjannet@gmail.com>
2021-01-11 16:20:17 +0100

    correcting output

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ a function that Sends a request and display a response """
 import requests
-from requests.exceptions import HTTPError
+from sys import argv
+if __name__ == "__main__":
     r = requests.get(argv[1])
         if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
+
