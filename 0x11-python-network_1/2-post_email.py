71a36b953a870c1a016243ffb8074eb32d9237b7
onsjannet <onsjannet@gmail.com>
2021-01-11 14:23:58 +0100

    correcting output

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ a function that fetches a URL """
 
-import urllib.parse
-import urllib.request
+import urllib.request as request
+import urllib.parse as parse
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values)
+    data = {'email': argv[2]}
+    data = parse.urlencode(data)
     data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
         print(res.read().decode('utf-8'))
