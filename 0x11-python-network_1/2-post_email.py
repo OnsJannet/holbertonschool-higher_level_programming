c3058bd93a738beeed8bf05f72fea46092c0bdd3
onsjannet <onsjannet@gmail.com>
2021-01-11 14:26:45 +0100

    pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    data = {'email' : argv[2]}
+    data = {'email': argv[2]}
     data = parse.urlencode(data)
     data = data.encode('ascii')
     r = request.Request(url, data)
