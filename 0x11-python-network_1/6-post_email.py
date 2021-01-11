ddee43a0d903331e6feb86ad2ce43d4fb4a333c2
onsjannet <onsjannet@gmail.com>
2021-01-11 14:00:00 +0100

    added a script that Post a request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" a function that sends a POST request """
+import requests
+from sys import argv
+if __name__ == "__main__":
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
+    
