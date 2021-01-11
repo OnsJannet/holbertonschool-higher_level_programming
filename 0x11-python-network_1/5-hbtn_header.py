ab6252e5c45a41bd1f07571d1a623c47016d1efa
onsjannet <onsjannet@gmail.com>
2021-01-11 13:45:41 +0100

    added a script that fetches a URL using request and sys

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+""" a function that fetches a URL """
+import requests
+from sys import argv
+if __name__ == "__main__":
+    r = requests.get((argv[1])
+    print(r.headers['X-Request-Id'])
