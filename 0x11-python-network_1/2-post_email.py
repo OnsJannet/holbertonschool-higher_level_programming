657fb22d39852d886ba18841c7ca0c7e31e5c30b
onsjannet <onsjannet@gmail.com>
2021-01-11 14:14:14 +0100

    added a script that returns a response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" a function that fetches a URL """
+
+import urllib.parse
+import urllib.request
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
