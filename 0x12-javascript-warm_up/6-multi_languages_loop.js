778e4ba555c3fd2f2ad01c081354b0a0d49ac2c3
onsjannet <onsjannet@gmail.com>
2021-02-02 18:43:37 +0100

    added a script that prints str in an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+// prints 3 phrases.
+
+const variable = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const phrase in variable) {
+  console.log(variable[phrase]);
+}
