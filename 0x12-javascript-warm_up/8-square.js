6eff55c343ea889fd8741f95276d4d0e66262089
onsjannet <onsjannet@gmail.com>
2021-02-02 19:51:21 +0100

    prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+// prints a square
+
+const count = process.argv[2];
+const character = 'x';
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let repeat = 0; repeat < count; repeat++) {
+    console.log(character.repeat(count));
+  }
+}
