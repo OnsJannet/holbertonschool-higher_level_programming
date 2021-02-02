fe86bab099518337f392534f16016a8cfeaa69f3
onsjannet <onsjannet@gmail.com>
2021-02-02 08:27:05 +0100

    loops

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+// prints a message depending of the number of arguments passed:
+// if no argument passed prints no argument
+// if arguments passed prints argument found
+
+let string = process.argv.length;
+if (string === 2) {
+    console.log('No argument');
+  } else if (string === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
