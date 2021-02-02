a236964ffceb1a6e1f681299bf0a57b7f60581c6
onsjannet <onsjannet@gmail.com>
2021-02-02 13:29:07 +0100

    added 3rd task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+// prints a message depending of the number of arguments passed:
+
+let string = process.argv[2];
+if (string === undefined) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }
