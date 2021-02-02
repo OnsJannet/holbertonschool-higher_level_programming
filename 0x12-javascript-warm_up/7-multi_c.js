cf1ac9d20841d2bd24befd42cb972fc2bd43a531
onsjannet <onsjannet@gmail.com>
2021-02-02 19:13:25 +0100

    repeates str x times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+// prints 3 phrases.
+
+const count = process.argv[2];
+if (parseInt(count)) {
+  console.log('C is fun \n'.repeat(count));
+} else {
+  console.log('Missing number of occurrences');
+}
