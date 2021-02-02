9b558f500a4babf745d9dfd9e130dcc8a11161d0
onsjannet <onsjannet@gmail.com>
2021-02-02 14:04:53 +0100

    semi

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,9 @@
 
 // prints a message depending of the number of arguments passed:
 
-let string = process.argv[2];
+const string = process.argv[2];
 if (string === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
