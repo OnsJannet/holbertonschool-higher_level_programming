8597f42fcb1093c60f4fdc9576bc458c2ecb9377
onsjannet <onsjannet@gmail.com>
2021-02-02 20:31:40 +0100

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+// factorial
+
+const interger = parseInt(process.argv[2]);
+function factorial (interger) {
+  if (interger === 1 || isNaN(interger)) {
+    return 1;
+  } else {
+    return (interger * factorial(interger - 1));
+  }
+}
+console.log(factorial(interger));
