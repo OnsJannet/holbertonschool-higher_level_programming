fecdfc847bef1d56d5dde4ba3c5fd85132954896
onsjannet <onsjannet@gmail.com>
2021-02-02 18:29:54 +0100

    correcting output

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 // prints a message depending of the number of arguments passed:
 
-const number = process.argv[2];
+let number = process.argv[2];
 if (parseInt(number)) {
   console.log('My number: ' + number);
 } else {
