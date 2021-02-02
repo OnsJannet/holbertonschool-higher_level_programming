4e2ca6a4a8e7ebe8a6617237ec6825b216b7a25c
onsjannet <onsjannet@gmail.com>
2021-02-02 19:55:05 +0100

    corrected output

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@
 const count = process.argv[2];
 const character = 'x';
 if (isNaN(count)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let repeat = 0; repeat < count; repeat++) {
     console.log(character.repeat(count));
