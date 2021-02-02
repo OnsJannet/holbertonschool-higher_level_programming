7bfc3576bae718684259b857beeb606133d13465
onsjannet <onsjannet@gmail.com>
2021-02-02 18:23:52 +0100

    added a script that checks a variable if it's an intger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+// prints a message depending of the number of arguments passed:
+
+const number = process.argv[2];
+if (parseInt(number)) {
+  console.log('My number: ' + number);
+} else {
+  console.log('Not a number');
+}
