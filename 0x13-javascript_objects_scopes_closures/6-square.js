6c68ec01e84a22e63acd114b9421945f53439fb3
onsjannet <onsjannet@gmail.com>
2021-02-08 13:04:07 +0100

    correcting output

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 // a class square that inherits from Rectangle
-class Square extends require('./5-square') {
+class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let count = 0; count < this.width; count++) {
-        console.log('C'.repeat(this.height));
+      for (let count = 0; count < this.height; count++) {
+        console.log('c'.repeat(this.width));
       }
     }
   }
