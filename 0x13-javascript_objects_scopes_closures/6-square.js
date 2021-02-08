7164eec56db4700876df625b1f38c5920add9193
onsjannet <onsjannet@gmail.com>
2021-02-08 09:15:07 +0100

    prints a square using C & X

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+// a class square that inherits from Rectangle
+class Square extends require('./4-rectangle') {
+    constructor (length) {
+      super(length, length);
+    }
+    charPrint(c){
+        if (c === undefined) {
+            this.print();
+          } else {
+            for (let count = 0; count < this.height; count++) {
+                console.log('C'.repeat(this.width));
+            }
+        }
+    }
+}
+  module.exports = Square;
