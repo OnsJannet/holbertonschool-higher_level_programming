f32df22e0b54934a7bec82b50c65de01b037d881
onsjannet <onsjannet@gmail.com>
2021-02-08 12:53:32 +0100

    semi correction

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,29 +3,27 @@
 // an empty class Rectangle that defines a rectangle
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let count = 0; count < this.height; count++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
-
-    rotate(){
-        [this.width, this.height] = [this.height, this.width];
-        return this.width, this.height
-
-    }
-
-    double(){
-        this.width *= 2
-        this.height *= 2
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    for (let count = 0; count < this.height; count++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+    return (this.width, this.height);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
