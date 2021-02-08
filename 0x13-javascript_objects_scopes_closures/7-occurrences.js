128e168d497e6b19f4e920a843e2945587921668
onsjannet <onsjannet@gmail.com>
2021-02-08 13:19:50 +0100

    added a script that returns the count of occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+
+//returns the number of occurences in a list
+
+exports.nbOccurences = function (list, searchElement){
+    return list.reduce((count, value) => (value === searchElement ? count + 1 : count), 0);
+}
