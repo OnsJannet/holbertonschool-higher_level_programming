0c4b6d808860d47cf76bc0a9710f848faf4d8a1e
onsjannet <onsjannet@gmail.com>
2021-02-08 13:27:52 +0100

    correcting semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-//returns the number of occurences in a list
+// returns the number of occurences in a list
+// https://www.w3resource.com/javascript-exercises/fundamental/javascript-fundamental-exercise-70.php
 
-exports.nbOccurences = function (list, searchElement){
-    return list.reduce((count, value) => (value === searchElement ? count + 1 : count), 0);
-}
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, value) => (value === searchElement ? count + 1 : count), 0);
+};
