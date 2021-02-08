f73d2bd1d42ba663bddae918b27fdbe475832620
onsjannet <onsjannet@gmail.com>
2021-02-08 07:45:02 +0100

    Rectangle class with width and height taking w and h values

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+// an empty class Rectangle that defines a rectangle
+// module.exports exports the class to useit seomwhere else
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
