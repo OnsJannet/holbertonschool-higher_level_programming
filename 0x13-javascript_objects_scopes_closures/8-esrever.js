4e8368a9b64ca4b62bfeb402aeb8b278bcf17b2d
onsjannet <onsjannet@gmail.com>
2021-02-08 13:43:05 +0100

    reverse a list without using .reverse()

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// returns the reversed version of a list
+exports.esrever = function (list) {
+  const newList = [];
+  for (let position = list.length - 1; position >= 0; position--) {
+    newList.push(list[position]);
+  }
+  return newList;
+};
