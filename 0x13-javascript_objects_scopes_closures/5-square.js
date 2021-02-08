23fc5d11027dc9c906626c58a63a6dbfb35fa9ff
onsjannet <onsjannet@gmail.com>
2021-02-08 09:00:13 +0100

    inheriting from another class

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+// a class square that inherits from Rectangle
+class Square extends require('./4-rectangle') {
+  constructor (length) {
+    super(length, length);
+  }
+}
+module.exports = Square;
