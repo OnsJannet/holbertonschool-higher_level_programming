b657492b885724781282a6d47057e25ccbc3937a
onsjannet <onsjannet@gmail.com>
2021-02-09 08:37:17 +0100

    added a script that writes on a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 
 //write a string to a file.
+
+/* File System Object */
+const fs = require('fs');
+/* Read File, write on a file */
+fs.writeFile(process.argv[2], process.argv[3], 'utf8',
+  function (error) {
+  /* If an error exists, show it*/
+    if (error) console.log(error || data);
+  });
