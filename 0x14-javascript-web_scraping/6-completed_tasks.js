bf064271553b6aa59caa2905a9366f9203715c6f
onsjannet <onsjannet@gmail.com>
2021-02-09 21:09:40 +0100

    added a script that counts completed todos

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,25 @@
 #!/usr/bin/node
 
-//compute the number of tasks completed by user id.
+// compute the number of tasks completed by user id.
+
+const url = process.argv[2];
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (const todo in todos) {
+      const task = todos[todo];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log(error);
+  }
+});
