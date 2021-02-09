05cb14f676c5abb2a79536f2f5b998396da4bada
onsjannet <onsjannet@gmail.com>
2021-02-09 19:13:23 +0100

    store a content of a page in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
 
-//get the contents of a webpage and stores it in a file.
+// get the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
+      if (error) console.log(error);
+    }
+    );
+  } else {
+    console.log(error);
+  }
+});
