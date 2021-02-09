e24d4ea00ab9c7757222f9950ae20973b958f2b3
onsjannet <onsjannet@gmail.com>
2021-02-09 08:28:56 +0100

    added a script that prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 
 // read and print the content of a file.
+
+/* File System Object */
+const fs = require('fs');
+/* Read File */
+fs.readFile(process.argv[2], 'utf8',
+  function (error, data) {
+  /* If an error exists, show it, otherwise show the file */
+    console.log(error || data);
+  });
