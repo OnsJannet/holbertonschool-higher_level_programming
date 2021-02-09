635c397b5eb3470cd0c8a743327004b1361566f5
onsjannet <onsjannet@gmail.com>
2021-02-09 08:56:25 +0100

    added a script that displays the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-//display the status code of a GET request.
+// display the status code of a GET request.
+
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  console.log(error || 'code: ' + response.statusCode);
+});
