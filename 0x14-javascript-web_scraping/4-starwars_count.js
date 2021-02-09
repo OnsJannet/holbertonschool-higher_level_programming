df842ee97b512dccef8ef98c2c97759c2d520d1f
onsjannet <onsjannet@gmail.com>
2021-02-09 18:29:33 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,23 @@
 #!/usr/bin/node
 
-/*print the number of movies where the character
+/* print the number of movies where the character
  “Wedge Antilles” is present. */
+
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (response.statusCode === 200) {
+    let count = 0;
+    const list = JSON.parse(body).results;
+    for (const people in list) {
+      const characters = list[people].characters;
+      for (const character in characters) {
+        if (characters[character].endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.log(error);
+  }
+});
