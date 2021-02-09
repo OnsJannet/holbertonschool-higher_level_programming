e02b129f5445d07dc63a0302ab9f03bc48afb466
onsjannet <onsjannet@gmail.com>
2021-02-09 13:19:45 +0100

    added a script that returns the title name

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
 //print the title of a Star Wars movie.
+
+url = 'https://swapi-api.hbtn.io/api/films/';
+const request = require('request');
+request(url, process.argv[2], function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
+});
