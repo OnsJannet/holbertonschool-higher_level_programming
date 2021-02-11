448a104f701a8b08a0d40124dcc205b5d3e23ff6
onsjannet <onsjannet@gmail.com>
2021-02-11 13:21:03 +0100

    added a script that adds a list element every time a tag is clicked

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+// adds a LI element to a list when the user clicks on the tag DIV#add_item
+
+const $ = window.$;
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
